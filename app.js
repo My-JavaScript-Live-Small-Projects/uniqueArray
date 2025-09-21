2a580a4a79f7f5f42a35f43c4e249e68ecae67ff
Azizullah <azizullahcodes@gmail.com>
2025-09-21 08:41:34 -0700

    update

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ function processing(){let getArray = document.getElementById('input').value;
     unique = unique.join(' ');
     display.value = unique;
 }
+
+// method 2 is more simple for unique array finding 
+/*let arr = ['a','b','b','c','d','d','e'];
+console.log(arr,typeof arr);
+let array = [];
+for(let i = 0 ;i < arr.length;i++){
+    if(arr[i] !== arr[i+1]){array.push(arr[i])}
+    else{arr[i] = array}
+}
+console.log(array)*/
